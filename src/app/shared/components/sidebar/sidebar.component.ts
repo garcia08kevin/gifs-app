26b7f53b7ff9b62cc8f66531328d282517b18770
Kevin <garciakevin0084@outlook.es>
2023-09-16 20:19:12 -0500

    first commit

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
+
+@Component({
+  selector: 'share-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+  constructor(private gifsService: GifsService) { }
+
+  get tags():string[]{
+    return this.gifsService.tagsHistory;
+  }
+
+  selectTag(tag:string):void{
+    this.gifsService.searchTag(tag)
+  }
+
+}
